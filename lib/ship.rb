d8e9d230f6db39b62e90ee19e8942e6217d6b96b
Josh Walsh <dmaggioarcangeli@yahoo.com>
2021-10-18 19:52:30 -0600

    Create class and initialize

---
+++ lib/ship.rb
@@ -0,0 +1,9 @@
+class Ship
+  attr_reader :name, :ship_size
+  attr_accessor :life
+  def initialize(name, ship_size)
+    @name = name
+    @ship_size = ship_size
+    @life = ship_size
+  end
+end 
