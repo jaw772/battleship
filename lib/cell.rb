5a50c86670af1c9787e1f1cf338830fa6c2cf013
Josh Walsh <dmaggioarcangeli@yahoo.com>
2021-10-18 21:42:37 -0600

    Update ship attribute

--- lib/cell.rb
+++ lib/cell.rb
@@ -3,9 +3,12 @@ require './lib/ship'
 class Cell
 
   attr_accessor :coords, :ship_position, :coordinate
-
+  attr_accessor :ship, :miss
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = ship
   end
 
-end 
+
+
+end
