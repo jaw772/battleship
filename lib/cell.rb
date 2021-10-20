967590edf8a0e63dc122a60a96beb8c1e7bc16b3
Josh Walsh <dmaggioarcangeli@yahoo.com>
2021-10-19 21:00:37 -0600

    Update Cell class

--- lib/cell.rb
+++ lib/cell.rb
@@ -6,43 +6,51 @@ class Cell
     @coordinate = coordinate
     @ship = ship
     @ship_position = ""
+    @shot_at = false
+    @empty = true
   end
 
   def place_ship(ship)
     @ship = ship
+    @empty = false
     @ship_position = @coordinate
     puts
   end
 
   def empty?
-    @ship_position == ""
+    @empty
+    #@ship_position == ""
   end
 
   def fired_upon?
-    @ship.health < @ship.length
+    if @shot_at == true #&& @coordinate == @ship_position
+      true
+    else
+      false
+    end
   end
 
   def fire_upon
-    if @coordinate = @ship_position
+    @shot_at = true
+    if @coordinate == @ship_position
       @ship.hit
+      @miss = false
     else
       @miss = true
+    end
   end
 
   def render(reveal = false)
     if reveal == true
-      puts "S"
-    end
-
-    if fired_upon? == true
+      "S"
+    elsif fired_upon? == true && @miss == false
       @ship_position = "H"
-    elsif fired_upon? == true && @ship.health = 0
+    elsif fired_upon? == true && @ship.sunk? == true
       @ship_position = "X"
-    elsif @miss == true
+    elsif @miss == true && @empty == true
       @coordinate = "M"
     else
       @coordinate = "."
     end
   end
-end
 end
