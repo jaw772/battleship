9bfdaec757d93c096cf368cb9f4dcb9c253cafd4
Joel Grant <joelmgrant@me.com>
2021-10-20 18:26:16 -0600

    Complete valid_coordinate?

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,12 @@
 class Board
-
+  attr_accessor :board
   def initialize()
-
+    @board = {}
   end
 
-  def validate_coordinate?
-    # TODO - Joel G.
+  def valid_coordinate?(coordinate)
+    coord_check = coordinate.split(//, 2)
+    (coord_check[0] <= "D") && (coord_check[1].to_i <= 4)
   end
 
 end
