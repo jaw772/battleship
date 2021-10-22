c319d0066ba96c9c0ec045bfa9edc54dc8cda2ed
Joel Grant <joelmgrant@me.com>
2021-10-22 15:41:44 -0600

    Add instance variables and place_ship

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,11 +1,20 @@
 def Computer
+  attr_accessor :board
 
   def initialize
-
+    @coordinates
+    @cells = CellGenerator.new
+    @board = Board.new(@cells)
   end
 
 
+  # computer places ships in random and VALID locations (exists and not occupied)
+  def place_ship
+    
+  end
+
   def fire
+
   end
 
   def already_fired?
