a74700a183f896b6a82d77f1069ad5b2c7d2ba7a
Kevin Gloss <kgloss89@gmail.com>
2021-10-21 22:00:02 -0600

    update valid placement for place method

--- lib/board.rb
+++ lib/board.rb
@@ -14,19 +14,23 @@ class Board
 
     # a valid placement will have the same number of cells as the ship length
     if check_cells.length == ship.length
+      if check_cells.all? {|cell| @cells[cell].empty?}
 
-      # a valid placement will have consecutive cells a1, a2, a3 or c1, c2, c3 for example
-      #this will check that the first letter of each guess is the same
-      if check_cells.sort.each_cons(2).all? {|one, two| one.ord == two.ord}
+        # a valid placement will have consecutive cells a1, a2, a3 or c1, c2, c3 for example
+        #this will check that the first letter of each guess is the same
+        if check_cells.sort.each_cons(2).all? {|one, two| one.ord == two.ord}
 
         #if first letter is the same checks if numbers are sequential
         seq_row = check_cells.map {|guess| guess[1]}
         seq_row.sort.each_cons(2).all? {|one, two| one.ord == two.ord - 1}
 
-      #this one will check that the first letters are sequential
-    elsif check_cells.sort.each_cons(2).all? {|one, two| one.ord == two.ord - 1}
+        #this one will check that the first letters are sequential
+        elsif check_cells.sort.each_cons(2).all? {|one, two| one.ord == two.ord - 1}
         #if first letters are sequential then we can check the number is the same
         check_cells.map {|guess| guess[1]}.uniq.length == 1
+        else
+          false
+        end
       else
         false
       end
