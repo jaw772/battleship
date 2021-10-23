937e55c3dd4fd1ce739299dac852016a318f4d66
Kevin Gloss <kgloss89@gmail.com>
2021-10-23 17:17:09 -0600

    Clean up

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -4,22 +4,14 @@ require './lib/cell_generator'
 require './lib/board'
 require './lib/interface'
 require './lib/computer'
-require './lib/turn'
 
-cruiser = Ship.new('Cruiser', 3)
-submarine = Ship.new('Submarine', 2)
-cells = CellGenerator.new.cells
-player_board = Board.new(cells)
-npc_board = Board.new(cells) #computer placing ships will need to effect this one
 
-computer = Computer.new
-turn = Turn.new(npc_board, player_board)
+player_cells = CellGenerator.new.cells
+player_board = Board.new(player_cells)
+npc_cells = CellGenerator.new.cells
+npc_board = Board.new(npc_cells)
+
 interface = Interface.new(npc_board, player_board)
 
 
-if interface.start_game? == 'P'
-  turn.take_turn
-
-
-#instead of evaluating the response to start game here, we can do it in turn
-#thinking that this is essentially all we may need in our runner file?
+interface.start_game?
