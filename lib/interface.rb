158c046b7cd37441bdd22039642e6608fb3f1d85
Kevin Gloss <kgloss89@gmail.com>
2021-10-23 17:16:33 -0600

    Debug and format output to user

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,36 +1,39 @@
 class Interface
-  attr_reader
 
-  #I think interface will only interact with player_turn and computer class
-  def initialize(player_board, npc_board)
+  def initialize(npc_board, player_board)
     @player_board = player_board
     @npc_board = npc_board
-    @cruiser = Ship.new("Cruiser", 3)
-    @submarine = Ship.new("Submarine", 2)
+    @npc_sub = Ship.new("Submarine", 2)
+    @npc_cruiser = Ship.new("Cruiser", 3)
+    @npc = Computer.new(@npc_board, @player_board, @npc_sub, @npc_cruiser)
+    @cruiser = Ship.new('Cruiser', 3)
+    @submarine = Ship.new('Submarine', 2)
   end
 
   def start_game?
-    puts "Welcome to BATTLESHIP \n Enter p to play. Enter q to quit."
+    puts "Welcome to BATTLESHIP \nEnter p to play. Enter q to quit."
     response = (gets.chomp).upcase
-    until response == 'P' || 'Q'
-        "Please enter valid option."
+    until response.eql?('P') || response.eql?('Q')
+      puts "Please enter valid option."
+      response = (gets.chomp).upcase
     end
-    if response == 'P'
-      "Yes! The game is afoot, or aboat, ah you get it."
+    if response.eql?('P')
+      puts "Yes! The game is afoot, or aboat, ah you get it."
       setup_ships
-    elsif'Q'
-      "Fine we didn't want to play with you anyways..."
+    elsif response.eql?('Q')
+      puts ''
+      puts "Fine we didn't want to play with you anyways..."
     end
   end
 
   def setup_ships
-    #@npc_board.place the npc will generate cells and put those into a board then place it's ships randomly
+    @npc.place_ships
     puts "My ships are already ready."
     puts "Just waiting on you to lay out your two ships."
     puts "The Cruiser is three units long and the Submarine is two units long."
-    @player_board.render
 
-    #the player will enter the coordinates they want for both ships below and they will be added to a pre-generated player board
+    puts @player_board.render
+
     puts "Enter the squares for the Cruiser (3 spaces):"
     place_in_cells = gets.chomp.upcase.split(' ')
 
@@ -54,18 +57,20 @@ class Interface
 
   def take_turn
     puts "The battle has begun!"
-    until (@npc_board.@cruiser.sunk? == true && @npc_board.@submarine.sunk? == true) || (@player_board.@cruiser.sunk? == true && @player_board.@submarine.sunk? == true) do
+
+    until (@npc_cruiser.sunk? == true && @npc_sub.sunk? == true) || (@cruiser.sunk? == true && @submarine.sunk? == true) do
       puts "=============COMPUTER BOARD============="
-      @npc_board.render
+      puts @npc_board.render
       puts "==============PLAYER BOARD=============="
-      @player_board.render(true)
+      puts @player_board.render(true)
 
       @completed_shot = false
       until @completed_shot == true
         puts "Enter the coordinate you would like to fire upon!"
-        @player_choice == (gets.chomp).upcase
-        if @npc_board.valid_coordinate?(@player_choice) == true
-          @fired_cell == @npc_board.cells[@player_choice]
+        @player_choice = (gets.chomp).upcase
+        puts ''
+        if @npc_board.valid_coordinate?([@player_choice]) == true
+          @fired_cell = @npc_board.cells[@player_choice]
           if @fired_cell.fired_upon? == false
             @fired_cell.fire_upon
             if @fired_cell.render == "M"
@@ -76,27 +81,33 @@ class Interface
               puts "Your shot on #{@player_choice} sunk the ship!"
             end
             @completed_shot = true
+            puts ''
           else
             puts "Your chosen coordinate has already been attacked. Please enter another coordinate."
+            puts ''
           end
         else
           puts "Please pick a valid coordinate."
         end
-        puts "================================================================"
+      end
+      puts "======================================="
+      puts ''
+      if @npc_cruiser.sunk? == false && @npc_sub.sunk? == false
         puts "Computer player fires!"
-        computer.fire(@player_board)
+        @npc.fire
       end
     end
 
-    puts "********** GAME OVER **********"
-
+    puts "************** GAME OVER ***************"
+    puts ''
     puts "=============COMPUTER BOARD============="
-    @npc_board.render(true)
+    puts @npc_board.render(true)
     puts "==============PLAYER BOARD=============="
-    @player_board.render(true)
+    puts @player_board.render(true)
 
-    if @npc_board.@cruiser.sunk? == true && @npc_board.@submarine.sunk? == true
+    if @npc_cruiser.sunk? == true && @npc_sub.sunk? == true
       puts "Congratulations! The computer has been defeated and you have won the game!!!"
+      puts ''
       start_game?
     else
       puts "Fortunately for the computer and unfortunately for you, the computer was won this battle!"
