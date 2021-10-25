3cebe8d252dacd41cb364bc8851db299bcc75276
Kevin Gloss <kgloss89@gmail.com>
2021-10-25 14:24:13 -0600

    Remove unnecessary variables

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -1,17 +1,5 @@
-require './lib/ship'
-require './lib/cell'
-require './lib/cell_generator'
-require './lib/board'
 require './lib/interface'
-require './lib/computer'
 
-
-player_cells = CellGenerator.new.cells
-player_board = Board.new(player_cells)
-npc_cells = CellGenerator.new.cells
-npc_board = Board.new(npc_cells)
-
-interface = Interface.new(npc_board, player_board)
-
+interface = Interface.new
 
 interface.start_game?
