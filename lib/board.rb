65994bb7a0a6b33dc1989b22f415682e97b27d92
Kevin Gloss <kgloss89@gmail.com>
2021-10-25 14:25:24 -0600

    Refactor valid placement method

--- lib/board.rb
+++ lib/board.rb
@@ -10,36 +10,27 @@ class Board
   end
 
   def valid_placement?(ship, check_cells = [])
-
-    # a valid placement will have the same number of cells as the ship length
     if check_cells.length == ship.length
       if check_cells.all? {|cell| @cells[cell].empty?}
-
-        # a valid placement will have consecutive cells a1, a2, a3 or c1, c2, c3 for example
-        #this will check that the first letter of each guess is the same
-        if check_cells.sort.each_cons(2).all? {|one, two| one.ord == two.ord}
-
-        #if first letter is the same checks if numbers are sequential
-          seq_row = check_cells.map {|guess| guess[1]}
-          seq_row.sort.each_cons(2).all? {|one, two| one.ord == two.ord - 1}
-
-        # true #see if this returns a true to check the interface conditions
-
-        #this one will check that the first letters are sequential
-        elsif check_cells.sort.each_cons(2).all? {|one, two| one.ord == two.ord - 1}
-        #if first letters are sequential then we can check the number is the same
-          check_cells.map {|guess| guess[1]}.uniq.length == 1
-
-        # true #see if this returns a true to check the interface conditions
-
-        else
-          false
-        end
-      else
-        false
+        place_in_row?(check_cells) || place_in_column?(check_cells)
       end
-    else
-      false
+    end
+  end
+
+  def place_in_row?(check_cells)
+    # this will check that the first letter of each guess is the same
+    if check_cells.sort.each_cons(2).all? {|one, two| one.ord == two.ord}
+      #if first letter is the same checks if numbers are sequential
+      seq_row = check_cells.map {|guess| guess[1]}
+      seq_row.sort.each_cons(2).all? {|one, two| one.ord == two.ord - 1}
+    end
+  end
+
+  def place_in_column?(check_cells)
+    #this will check that the first letters are sequential
+    if check_cells.sort.each_cons(2).all? {|one, two| one.ord == two.ord - 1}
+      #if first letters are sequential then we can check the number is the same
+      check_cells.map {|guess| guess[1]}.uniq.length == 1
     end
   end
 
