79bcebe02fe1c4e96b4ff75ae1135a9c1faf4e11
Josh Walsh <dmaggioarcangeli@yahoo.com>
2021-10-26 15:43:30 -0600

    Add ability for larger board

--- lib/cell_generator.rb
+++ lib/cell_generator.rb
@@ -5,11 +5,16 @@ class CellGenerator
 
   def initialize(coordinates = Hash.new)
     @coordinates = coordinates
+    @alphabet  = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+      "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
   end
 
-  def cells
-    rows = ('A'..'D').to_a
-    columns = (('1'..'4').to_a)
+  def cells(row, column)
+    @row = row.to_i
+    @column = column
+    letter = @row - 1
+    rows = ('A'..@alphabet[letter]).to_a
+    columns = (('1'..@column).to_a)
     grid = rows.product(columns)
     grid = grid.map {|square| square.join}
     grid.each do |square|
